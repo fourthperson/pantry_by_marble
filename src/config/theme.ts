24141622e6ae84d57c1a4e82dcdb7ddb6f4eb4e5
fourthperson <iankariithi@gmail.com>
2025-01-21 19:49:32 +0300

    splash screen

--- src/config/theme.ts
+++ src/config/theme.ts
@@ -1,3 +1,7 @@
 // colors
 export const bgColor = '#FCF9F5';
 export const primaryColor = '#54634B';
+
+// fonts
+export const serifRegular = 'Adobe Garamond Pro';
+export const serifBold = 'Adobe Garamond Pro Bold';
--- src/screens/splash/Splash.tsx
+++ src/screens/splash/Splash.tsx
@@ -1,22 +1,39 @@
 import React from 'react';
 import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
-import {bgColor} from "../../config/theme.ts";
+import {bgColor, primaryColor, serifBold} from '../../config/theme.ts';
 
 function SplashScreen(): React.JSX.Element {
     return (
         <View style={styles.container}>
-            <SafeAreaView>
-                <Text>Splash Screen</Text>
+            <SafeAreaView style={styles.centered}>
+                <Text style={styles.brandText}>Pantry</Text>
+                <Text style={styles.brandSubText}>by Marble</Text>
             </SafeAreaView>
         </View>
     );
 }
 
-export default SplashScreen;
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: bgColor
+        backgroundColor: bgColor,
+    },
+    centered: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    brandText: {
+        fontSize: 48,
+        fontFamily: serifBold,
+        color: primaryColor,
+    },
+    brandSubText: {
+        fontSize: 24,
+        fontFamily: serifBold,
+        color: primaryColor,
     },
 });
+
+export default SplashScreen;
